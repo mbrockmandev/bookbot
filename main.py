bce9cde3a5e425ef84035caef1336b0d11ea4ff2
Michael Brockman <mbrockmandev@gmail.com>
2023-06-06 20:56:08 -0400

    add letter counting and sorting of output

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
-print("Hello world!")
+def count_letters(string: str):
+    my_dict = {}
+    for c in string:
+        c_lowered = c.lower()
+        if c_lowered.isalpha():
+            if c_lowered in my_dict:
+                my_dict[c_lowered] += 1
+            else:
+                my_dict[c_lowered] = 1
+    output = sorted(my_dict.items(), key=lambda x: x[1], reverse=True)
+    for k, v in output:
+        print(f"the '{k}' character was found {v} times")
+
 
 with open("frankenstein.txt") as f:
     file_contents = f.read()
@@ -6,3 +18,5 @@ with open("frankenstein.txt") as f:
 print(file_contents)
 words = file_contents.split()
 print(f"{len(words)} words")
+
+count_letters(file_contents)
